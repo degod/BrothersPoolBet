d80f21cfab42fcbb7b7666298b0461fff37607db
Dharmmielolah <dharmmie5@gmail.com>
2017-03-02 22:10:54 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,29 @@
+<header>
+    
+<nav class="navbar">
+  <div class="container-fluid">
+    <div class="navbar-header">      
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="index.php"><img src="./images/logo.png" style="width: 280px; height: 40px"></a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+        <!-- <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">SPORT</a></li>
+            <li><a href="#">LIVE</a></li>
+        </ul> -->
+        <ul class="nav navbar-nav navbar-right">
+           <!-- <li><a href=""><span class="glyphicon glyphicon-log-in"></span> Register</a></li>
+          <li><a href="#jp"><span class="glyphicon glyphicon-log-in"></span> Log-in</a></li> -->
+            <input class="form-control hA" type="text" placeholder="Username">
+            <input class="form-control hA" type="password" placeholder="Password">
+            <button class="btn btn-success login hA"><span class="glyphicon glyphicon glyphicon-send"></span></button>
+        </ul>
+    </div>
+  </div>
+</nav>
+    
+</header>
